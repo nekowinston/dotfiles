c9711d316ed91b27c18a92c27473fb767c15aa98
winston <hey@winston.sh>
2022-07-31 10:04:59 +0200

    fix(sketchybar): sound plugin - use same icons as polybar

--- dot_config/sketchybar/plugins/executable_sound.sh
+++ dot_config/sketchybar/plugins/executable_sound.sh
@@ -4,21 +4,21 @@ VOLUME=$(osascript -e "get volume settings" | cut -d " " -d ":" -f2 | cut -d ","
 MUTED=$(osascript -e "get volume settings" | grep "muted:true")
 
 if [[ "$MUTED" != "" ]]; then
-ICON="ﱝ"
+ICON="ﱝ "
 else
 case ${VOLUME} in
-  100) ICON="";;
-  9[0-9]) ICON="";;
-  8[0-9]) ICON="";;
-  7[0-9]) ICON="";;
-  6[0-9]) ICON="";;
-  5[0-9]) ICON="";;
-  4[0-9]) ICON="";;
-  3[0-9]) ICON="";;
-  2[0-9]) ICON="";;
-  1[0-9]) ICON="";;
-  [0-9]) ICON="";;
-  *) ICON=""
+  100) ICON="墳";;
+  9[0-9]) ICON="墳";;
+  8[0-9]) ICON="墳";;
+  7[0-9]) ICON="墳";;
+  6[0-9]) ICON="奔";;
+  5[0-9]) ICON="奔";;
+  4[0-9]) ICON="奔";;
+  3[0-9]) ICON="奔";;
+  2[0-9]) ICON="奄";;
+  1[0-9]) ICON="奄";;
+  [0-9]) ICON="奄";;
+  *) ICON="奄"
 esac
 fi
 
