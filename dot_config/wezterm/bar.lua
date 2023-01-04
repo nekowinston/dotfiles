8670166cba0326bc69ac03c9f2dd05bc91c9392c
winston <hey@winston.sh>
2023-01-04 13:22:00 +0100

    style(wezterm): reformat with stylua

--- dot_config/wezterm/bar.lua
+++ dot_config/wezterm/bar.lua
@@ -50,80 +50,86 @@ end
 
 -- custom tab bar
 ---@diagnostic disable-next-line: unused-local
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local colours = config.resolved_palette.tab_bar
+wezterm.on(
+  "format-tab-title",
+  function(tab, tabs, panes, config, hover, max_width)
+    local colours = config.resolved_palette.tab_bar
 
-  local active_tab_index = 0
-  for _, t in ipairs(tabs) do
-    if t.is_active == true then
-      active_tab_index = t.tab_index
+    local active_tab_index = 0
+    for _, t in ipairs(tabs) do
+      if t.is_active == true then
+        active_tab_index = t.tab_index
+      end
     end
-  end
 
-  local rainbow = {
-    config.resolved_palette.ansi[2],
-    config.resolved_palette.indexed[16],
-    config.resolved_palette.ansi[4],
-    config.resolved_palette.ansi[3],
-    config.resolved_palette.ansi[5],
-    config.resolved_palette.ansi[6]
-  }
+    local rainbow = {
+      config.resolved_palette.ansi[2],
+      config.resolved_palette.indexed[16],
+      config.resolved_palette.ansi[4],
+      config.resolved_palette.ansi[3],
+      config.resolved_palette.ansi[5],
+      config.resolved_palette.ansi[6],
+    }
 
-  local i = (tab.tab_index) % 6
-  local active_bg = rainbow[i + 1]
-  local active_fg = colours.background
-  local inactive_bg = colours.inactive_tab.bg_color
-  local inactive_fg = colours.inactive_tab.fg_color
-  local new_tab_bg = colours.new_tab.bg_color
+    local i = tab.tab_index % 6
+    local active_bg = rainbow[i + 1]
+    local active_fg = colours.background
+    local inactive_bg = colours.inactive_tab.bg_color
+    local inactive_fg = colours.inactive_tab.fg_color
+    local new_tab_bg = colours.new_tab.bg_color
 
-  local s_bg, s_fg, e_bg, e_fg
+    local s_bg, s_fg, e_bg, e_fg
 
-  -- the last tab
-  if tab.tab_index == #tabs - 1 then
-    if tab.is_active then
+    -- the last tab
+    if tab.tab_index == #tabs - 1 then
+      if tab.is_active then
+        s_bg = active_bg
+        s_fg = active_fg
+        e_bg = new_tab_bg
+        e_fg = active_bg
+      else
+        s_bg = inactive_bg
+        s_fg = inactive_fg
+        e_bg = new_tab_bg
+        e_fg = inactive_bg
+      end
+    elseif tab.tab_index == active_tab_index - 1 then
+      s_bg = inactive_bg
+      s_fg = inactive_fg
+      e_bg = rainbow[(i + 1) % 6 + 1]
+      e_fg = inactive_bg
+    elseif tab.is_active then
       s_bg = active_bg
       s_fg = active_fg
-      e_bg = new_tab_bg
+      e_bg = inactive_bg
       e_fg = active_bg
     else
       s_bg = inactive_bg
       s_fg = inactive_fg
-      e_bg = new_tab_bg
+      e_bg = inactive_bg
       e_fg = inactive_bg
     end
-  elseif tab.tab_index == active_tab_index - 1 then
-    s_bg = inactive_bg
-    s_fg = inactive_fg
-    e_bg = rainbow[(i + 1) % 6 + 1]
-    e_fg = inactive_bg
-  elseif tab.is_active then
-    s_bg = active_bg
-    s_fg = active_fg
-    e_bg = inactive_bg
-    e_fg = active_bg
-  else
-    s_bg = inactive_bg
-    s_fg = inactive_fg
-    e_bg = inactive_bg
-    e_fg = inactive_bg
+
+    local tabi = wezterm.mux.get_tab(tab.tab_id)
+    local muxpanes = tabi:panes()
+    local count = #muxpanes == 1 and "" or #muxpanes
+    local index = tab.tab_index + 1 .. ": "
+
+    return {
+      { Background = { Color = s_bg } },
+      { Foreground = { Color = s_fg } },
+      {
+        Text = " " .. index .. tab.active_pane.title .. numberStyle(
+          count,
+          "superscript"
+        ) .. " ",
+      },
+      { Background = { Color = e_bg } },
+      { Foreground = { Color = e_fg } },
+      { Text = DIVIDERS.RIGHT },
+    }
   end
-
-  local tabi = wezterm.mux.get_tab(tab.tab_id)
-  local muxpanes = tabi:panes()
-  local count = #muxpanes == 1 and "" or #muxpanes
-  local index = tab.tab_index + 1 .. ": "
-
-  return {
-    { Background = { Color = s_bg } },
-    { Foreground = { Color = s_fg } },
-    {
-      Text = " " .. index .. tab.active_pane.title .. numberStyle(count, "superscript") .. " ",
-    },
-    { Background = { Color = e_bg } },
-    { Foreground = { Color = e_fg } },
-    { Text = DIVIDERS.RIGHT },
-  }
-end)
+)
 
 local function arrContains(arr, val)
   for _, v in ipairs(arr) do
@@ -139,10 +145,17 @@ local nonpadded_apps = { "nvim", "btop", "btm" }
 
 wezterm.on("smartpadding", function(window, pane)
   local fgp = pane:get_foreground_process_info()
-  if fgp == nil then return
+  if fgp == nil then
+    return
   elseif arrContains(nonpadded_apps, fgp.name) then
-    window:set_config_overrides({ window_padding = { left = 0, right = 0, top = 0, bottom = 0 } })
-  else window:set_config_overrides({ window_padding = wezterm.GLOBAL.smart_padding }) end
+    window:set_config_overrides({
+      window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+    })
+  else
+    window:set_config_overrides({
+      window_padding = wezterm.GLOBAL.smart_padding,
+    })
+  end
 end)
 
 -- custom status
@@ -156,9 +169,12 @@ wezterm.on("update-status", function(window, pane)
   local first_tab_active = window:mux_window():tabs_with_info()[1].is_active
 
   local leader_text = "   "
-  if window:leader_is_active() then leader_text = "   " end
+  if window:leader_is_active() then
+    leader_text = "   "
+  end
 
-  local divider_bg = first_tab_active and palette.ansi[2] or palette.tab_bar.inactive_tab.bg_color
+  local divider_bg = first_tab_active and palette.ansi[2]
+    or palette.tab_bar.inactive_tab.bg_color
 
   window:set_left_status(wezterm.format({
     { Foreground = { Color = palette.background } },
--- dot_config/wezterm/theme.lua
+++ dot_config/wezterm/theme.lua
@@ -3,31 +3,31 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.scheme_for_appearance = function(appearance, theme)
-	if string.match(wezterm.target_triple, "linux") then
-		return theme.dark
-	end
-	if appearance:find("Dark") then
-		return theme.dark
-	else
-		return theme.light
-	end
+  if string.match(wezterm.target_triple, "linux") then
+    return theme.dark
+  end
+  if appearance:find("Dark") then
+    return theme.dark
+  else
+    return theme.light
+  end
 end
 
 M.get_custom_colorschemes = function()
-	local oledppuccin = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
-	oledppuccin.background = "#000000"
-	oledppuccin.tab_bar.background = "#040404"
-	oledppuccin.tab_bar.inactive_tab.bg_color = "#0f0f0f"
-	oledppuccin.tab_bar.new_tab.bg_color = "#080808"
+  local oledppuccin = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+  oledppuccin.background = "#000000"
+  oledppuccin.tab_bar.background = "#040404"
+  oledppuccin.tab_bar.inactive_tab.bg_color = "#0f0f0f"
+  oledppuccin.tab_bar.new_tab.bg_color = "#080808"
   oledppuccin.ansi[6] = "#c6a0f6"
 
-	local latte = wezterm.color.get_builtin_schemes()["Catppuccin Latte"]
+  local latte = wezterm.color.get_builtin_schemes()["Catppuccin Latte"]
   latte.ansi[6] = "#8839ef"
 
-	return {
-		["OLEDppuccin"] = oledppuccin,
+  return {
+    ["OLEDppuccin"] = oledppuccin,
     ["Catppuccin Latte"] = latte,
-	}
+  }
 end
 
 return M
