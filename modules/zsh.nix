ea687fe8135356d0ba7b8c9f50138a2097a0f019
winston <hey@winston.sh>
2023-02-08 10:04:55 +0100

    fix: use github zsh-nix-shell to make it work

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -43,6 +43,7 @@
 
       envExtra = ''
         export PATH="$PATH:${config.xdg.dataHome}/krew/bin"
+        export ZVM_INIT_MODE=sourcing
       '';
 
       dotDir = ".config/zsh";
@@ -64,8 +65,13 @@
         }
         {
           name = "zsh-nix-shell";
-          src = pkgs.zsh-nix-shell;
           file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.5.0";
+            sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
+          };
         }
       ];
       shellAliases = {
