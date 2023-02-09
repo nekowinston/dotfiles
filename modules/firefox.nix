a8745d32b42c8c6ec5effcdbde72251ec80d8515
winston <hey@winston.sh>
2023-02-09 17:34:07 +0100

    feat(firefox): set DoT DNS

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -111,7 +111,9 @@ in
         "network.predictor.enable-prefetch" = false;
         "network.predictor.enabled" = false;
         "network.prefetch-next" = false;
-        "network.trr.mode" = 5;
+        "network.trr.bootstrapAddress" = "146.255.56.98";
+        "network.trr.custom_uri" = "https://doh.applied-privacy.net/query";
+        "network.trr.mode" = 2;
         "pdfjs.enableScripting" = false;
         "privacy.donottrackheader.enabled" = true;
         "privacy.donottrackheader.value" = 1;
