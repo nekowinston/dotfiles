55a97145f675b53ae51f82ee2dc71eb9a3ceac12
winston <hey@winston.sh>
2023-02-14 00:18:43 +0100

    feat(nix): enable garbage collection/store optimisation

--- machines/common.nix
+++ machines/common.nix
@@ -1,20 +1,24 @@
 {
-  nix.settings = {
-    substituters = [
-      "https://catppuccin.cachix.org"
-      "https://hyprland.cachix.org"
-      "https://mic92.cachix.org"
-      "https://nekowinston.cachix.org"
-      "https://nix-community.cachix.org"
-      "https://pre-commit-hooks.cachix.org"
-    ];
-    trusted-public-keys = [
-      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
-      "nekowinston.cachix.org-1:lucpmaO+JwtoZj16HCO1p1fOv68s/RL1gumpVzRHRDs="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
-    ];
+  nix = {
+    gc.automatic = true;
+    settings = {
+      auto-optimise-store = true;
+      substituters = [
+        "https://catppuccin.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://mic92.cachix.org"
+        "https://nekowinston.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://pre-commit-hooks.cachix.org"
+      ];
+      trusted-public-keys = [
+        "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "mic92.cachix.org-1:gi8IhgiT3CYZnJsaW7fxznzTkMUOn1RY4GmXdT/nXYQ="
+        "nekowinston.cachix.org-1:lucpmaO+JwtoZj16HCO1p1fOv68s/RL1gumpVzRHRDs="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+      ];
+    };
   };
 }
