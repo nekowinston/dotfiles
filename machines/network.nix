78df3dc15bca67e3afcbb96c6517789eb07ca871
winston <hey@winston.sh>
2023-03-01 20:47:27 +0100

    fix(network): split network config to avoid inf recursion

--- machines/network.nix
+++ machines/network.nix
@@ -2,62 +2,45 @@
   lib,
   pkgs,
   ...
-}: let
-  inherit (pkgs.stdenv.hostPlatform) isLinux;
-in {
-  services =
-    {
-      dnsmasq =
-        if isLinux
-        then {
-          enable = true;
-          servers = [
-            "::1#53000"
-            "127.0.0.1#53000"
-          ];
-          extraConfig = ''
-            # stubby
-            no-resolv
-            proxy-dnssec
-            listen-address=::1,127.0.0.1
+}: {
+  services = {
+    dnsmasq = {
+      enable = true;
+      servers = [
+        "::1#53000"
+        "127.0.0.1#53000"
+      ];
+      extraConfig = ''
+        # stubby
+        no-resolv
+        proxy-dnssec
+        listen-address=::1,127.0.0.1
 
-            # loopback for development
-            address=/test/127.0.0.1
-          '';
-        }
-        # nix-darwin config
-        else {
-          enable = true;
-          addresses."test" = "127.0.0.1";
-          bind = "127.0.0.1";
-        };
-    }
-    // (
-      if isLinux
-      then {
-        mullvad-vpn = {
-          enable = true;
-        };
-        stubby = {
-          enable = true;
-          settings = {
-            resolution_type = "GETDNS_RESOLUTION_STUB";
-            listen_addresses = ["127.0.0.1@53000" "0::1@53000"];
-            upstream_recursive_servers = [
-              {
-                address_data = "194.242.2.3";
-                tls_port = 853;
-                tls_auth_name = "adblock.doh.mullvad.net";
-              }
-              {
-                address_data = "2a07:e340::3";
-                tls_port = 853;
-                tls_auth_name = "adblock.doh.mullvad.net";
-              }
-            ];
-          };
-        };
-      }
-      else {}
-    );
+        # loopback for development
+        address=/test/127.0.0.1
+      '';
+    };
+    mullvad-vpn = {
+      enable = true;
+    };
+    stubby = {
+      enable = true;
+      settings = {
+        resolution_type = "GETDNS_RESOLUTION_STUB";
+        listen_addresses = ["127.0.0.1@53000" "0::1@53000"];
+        upstream_recursive_servers = [
+          {
+            address_data = "194.242.2.3";
+            tls_port = 853;
+            tls_auth_name = "adblock.doh.mullvad.net";
+          }
+          {
+            address_data = "2a07:e340::3";
+            tls_port = 853;
+            tls_auth_name = "adblock.doh.mullvad.net";
+          }
+        ];
+      };
+    };
+  };
 }
--- machines/sashimi/default.nix
+++ machines/sashimi/default.nix
@@ -29,5 +29,10 @@
   services = {
     # Auto upgrade nix package and the daemon service.
     nix-daemon.enable = true;
+    dnsmasq = {
+      enable = true;
+      addresses."test" = "127.0.0.1";
+      bind = "127.0.0.1";
+    };
   };
 }
