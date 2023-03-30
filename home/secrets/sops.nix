54643990dd02e15e099e52f96ed94c5c644afc11
winston <hey@winston.sh>
2023-03-30 06:12:06 +0200

    feat(sops): use konf for kubeconfig storage

--- home/secrets/sops.nix
+++ home/secrets/sops.nix
@@ -7,7 +7,10 @@
     gnupg.home = config.programs.gpg.homedir;
     defaultSopsFile = ./main.yaml;
     secrets = {
-      "kubeconfig".path = "${config.xdg.configHome}/kube/kubeconfig";
+      "konf-ctp".path = "${config.home.homeDirectory}/.kube/konfs/store/ctp_ctp.yaml";
+      "konf-fra1".path = "${config.home.homeDirectory}/.kube/konfs/store/fra1_fra1.yaml";
+      "konf-work-prod".path = "${config.home.homeDirectory}/.kube/konfs/store/work-prod_work-prod.yaml";
+      "konf-work-staging".path = "${config.home.homeDirectory}/.kube/konfs/store/work-staging_work-staging.yaml";
     };
   };
 }
