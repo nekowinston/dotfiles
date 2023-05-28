704ec27d661523b0305976481d0aa89cc32d44d8
winston <hey@winston.sh>
2023-05-28 19:21:54 +0200

    feat: manage global zsh on nix-darwin
    ty @NyxKrage

--- machines/sashimi/default.nix
+++ machines/sashimi/default.nix
@@ -12,6 +12,8 @@
     hostName = name;
   };
 
+  programs.zsh.enable = true;
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
