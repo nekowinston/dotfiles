930e5b45764ade54badaab039fe6e9b457ab6477
winston <hey@winston.sh>
2023-06-19 22:27:04 +0200

    feat: set macOS wallpaper

--- home/default.nix
+++ home/default.nix
@@ -63,6 +63,27 @@ in {
     taskwarrior.enable = true;
   };
 
+  home.activation.set-wallpaper = let
+    wallpaper = ./wallpapers/dhm_1610.png;
+    killall = lib.getExe pkgs.killall;
+    sqlite = lib.getExe pkgs.sqlite;
+  in
+    lib.mkIf isDarwin (lib.hm.dag.entryAfter ["writeBoundary"] ''
+      dpdb="$HOME/Library/Application Support/Dock/desktoppicture.db"
+
+      ${sqlite} "$dpdb" "insert into data values ('${wallpaper}');"
+      new_entry=$(${sqlite} "$dpdb" "select max(rowid) from data;")
+      pics=$(${sqlite} "$dpdb" "select rowid from pictures")
+
+      ${sqlite} "$dpdb" "delete from preferences;"
+
+      for pic in $pics; do
+        ${sqlite} "$dpdb" "insert into preferences (key, data_id, picture_id) values(1, $new_entry, $pic)"
+      done
+
+      ${killall} Dock
+    '');
+
   xdg.configFile."ideavim/ideavimrc".source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/apps/ideavim/ideavimrc";
   xdg.configFile."Yubico/u2f_keys".text = ''
     winston:+SzANNyl5RpjNZFCthItSi7rQgiNqKNQztm2omNDnMOMNYXbnpoxMY/tqNCqoUtcAnkSmfC1/2E3WMZZ+IupFw==,gw1FnUrGJ2/vsxrcyOP17603yWSSk2OaatqvqkzhiEmRd/FAzWuXYE2YA16SBB9n+f6IypjerPgwY06zOw3DOA==,es256,+presence%
