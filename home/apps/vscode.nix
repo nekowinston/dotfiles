1012e0cfdf4aac8a4963a095a91db81bd5108c0e
winston <hey@winston.sh>
2023-06-20 20:09:18 +0200

    feat(vscode): add more nix extensions, keybindings

--- home/apps/vscode.nix
+++ home/apps/vscode.nix
@@ -1,36 +1,49 @@
 {
   config,
   flakePath,
+  lib,
   pkgs,
   ...
 }: let
   inherit (pkgs.stdenv.hostPlatform) isDarwin isLinux;
+
+  settingsJSON = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/apps/vscode/settings.json";
+  keybindingsJSON = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/apps/vscode/keybindings.json";
 in {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
     extensions = with pkgs.vscode-extensions.vscode-marketplace; [
+      adrianwilczynski.alpine-js-intellisense
+      bradlc.vscode-tailwindcss
+      catppuccin.catppuccin-vsc-icons
+      denoland.vscode-deno
+      eamodio.gitlens
+      esbenp.prettier-vscode
+      github.vscode-pull-request-github
+      gitlab.gitlab-workflow
       jnoortheen.nix-ide
+      lunuan.kubernetes-templates
       mkhl.direnv
       ms-kubernetes-tools.vscode-kubernetes-tools
       pkief.material-icon-theme
       redhat.vscode-yaml
       rust-lang.rust-analyzer
+      sumneko.lua
       tamasfe.even-better-toml
       valentjn.vscode-ltex
       vscodevim.vim
     ];
     mutableExtensionsDir = true;
   };
-  home.file = {
-    "${config.xdg.configHome}/Code/User/settings.json" = {
-      enable = isLinux;
-      source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/apps/vscode/settings.json";
-    };
-    "Library/Application Support/Code/User/settings.json" = {
-      enable = isDarwin;
-      source = config.lib.file.mkOutOfStoreSymlink "${flakePath}/home/apps/vscode/settings.json";
-    };
+
+  home.file = lib.mkIf isDarwin {
+    "Library/Application Support/Code/User/keybindings.json".source = keybindingsJSON;
+    "Library/Application Support/Code/User/settings.json".source = settingsJSON;
+  };
+  xdg.configFile = lib.mkIf isLinux {
+    "Code/User/keybindings.json".source = keybindingsJSON;
+    "Code/User/settings.json".source = settingsJSON;
   };
   xdg.mimeApps.defaultApplications."text/plain" = "code.desktop";
 }
