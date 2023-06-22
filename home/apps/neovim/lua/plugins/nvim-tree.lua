863b036bef4e31ade942dad278c9f7af4ad8585b
winston <hey@winston.sh>
2023-06-22 23:17:12 +0200

    feat(nvim): add icons for devicons

--- home/apps/neovim/lua/plugins/nvim-tree.lua
+++ home/apps/neovim/lua/plugins/nvim-tree.lua
@@ -23,4 +23,50 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
   },
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = function()
+      local devicons = require("nvim-web-devicons")
+      local justfile = {
+        icon = "󱚣 ",
+        name = "Justfile",
+        color = "#dea584",
+      }
+      devicons.setup({
+        override_by_extension = {
+          ["norg"] = {
+            icon = " ",
+            name = "Neorg",
+            color = "#77aa99",
+          },
+        },
+        override_by_filename = {
+          [".ecrc"] = {
+            icon = " ",
+            name = "EditorConfigChecker",
+            color = "#a6e3a1",
+          },
+          [".envrc"] = {
+            icon = " ",
+            name = "envrc",
+            color = "#faf743",
+          },
+          [".editorconfig"] = {
+            icon = " ",
+            name = "EditorConfig",
+            color = "#a6e3a1",
+          },
+          [".luacheckrc"] = {
+            icon = "󰢱 ",
+            name = "LuacheckRC",
+            color = "#51a0cf",
+          },
+          [".Justfile"] = justfile,
+          [".justfile"] = justfile,
+          ["Justfile"] = justfile,
+          ["justfile"] = justfile,
+        },
+      })
+    end,
+  },
 }
