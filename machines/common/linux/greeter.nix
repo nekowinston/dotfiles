4e6c0940f21a29cc90290e4feab9ff3f78759fdc
winston <hey@winston.sh>
2023-07-18 04:24:38 +0200

    feat(greeter): regreet -> tuigreet

--- machines/common/linux/greeter.nix
+++ machines/common/linux/greeter.nix
@@ -3,40 +3,21 @@
   lib,
   pkgs,
   ...
-}: let
-  greetdConfig = pkgs.writeText "greetd-config" ''
-    output "*" {
-      scale 2
-    }
-    exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
-    seat seat0 xcursor_theme "macOS Monterey" 24
-    xwayland disable
-    exec "${lib.getExe config.programs.regreet.package}; swaymsg exit"
-  '';
-in {
-  programs.regreet = {
+}: {
+  services.greetd = {
     enable = true;
-    settings = {
-      background = {
-        path = ../../../home/wallpapers/dhm_1610.png;
-        fit = "Cover";
-      };
-      GTK = {
-        font_name = "IBM Plex Mono 16";
-        cursor_theme_name = "macOS Monterey";
-        icon_theme_name = "Papirus-Dark";
-        theme_name = "Catppuccin-Mocha-Compact-Pink-Dark";
-      };
+    settings.default_session = {
+      command = lib.concatStringsSep " " [
+        "${pkgs.greetd.tuigreet}/bin/tuigreet"
+        "--remember"
+        "--remember-user-session"
+        "--sessions=${config.programs.sway.package}/share/wayland-sessions"
+      ];
+      user = "greeter";
     };
   };
-  services.greetd.settings.default_session.command = "${lib.getExe config.programs.sway.package} --config ${greetdConfig}";
-  services.gnome.gnome-keyring.enable = true;
 
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    xwayland.hidpi = true;
-  };
+  services.gnome.gnome-keyring.enable = true;
 
   security.pam = {
     u2f = {
@@ -68,17 +49,4 @@ in {
       };
     };
   };
-  environment.systemPackages = with pkgs; [
-    (catppuccin-gtk.override {
-      accents = ["pink"];
-      variant = "mocha";
-      size = "compact";
-    })
-    (catppuccin-papirus-folders.override {
-      flavor = "mocha";
-      accent = "pink";
-    })
-    pkgs.nur.repos.nekowinston.apple-cursor
-    xorg.xprop
-  ];
 }
