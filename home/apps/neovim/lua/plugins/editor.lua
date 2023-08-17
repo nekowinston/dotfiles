179e7b2dc91a164dfacca1db18b2b1a0240c7cf7
winston <hey@winston.sh>
2023-08-17 04:24:02 +0200

    feat(nvim): lightspeed.nvim -> flash.nvim

--- home/apps/neovim/lua/plugins/editor.lua
+++ home/apps/neovim/lua/plugins/editor.lua
@@ -5,7 +5,33 @@ return {
   -- respect project settings
   "gpanders/editorconfig.nvim",
   -- best hop
-  "ggandor/lightspeed.nvim",
+  {
+    "folke/flash.nvim",
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      {
+        "s", mode = { "n", "x", "o" }, desc = "Flash",
+        function() require("flash").jump() end,
+      },
+      {
+        "S", mode = { "n", "o", "x" }, desc = "Flash Treesitter",
+        function() require("flash").treesitter() end,
+      },
+      {
+        "r", mode = "o", desc = "Remote Flash",
+        function() require("flash").remote() end,
+      },
+      {
+        "R", mode = { "o", "x" }, desc = "Treesitter Search",
+        function() require("flash").treesitter_search() end,
+      },
+      {
+        "<c-f>", mode = { "c" }, desc = "Toggle Flash Search",
+        function() require("flash").toggle() end,
+      },
+    },
+  },
   -- run tasks
   "skywind3000/asyncrun.vim",
   "skywind3000/asynctasks.vim",
