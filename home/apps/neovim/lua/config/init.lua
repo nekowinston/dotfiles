0d2d83c167a00da98f036a897e8e8f26c5b0bad0
winston <hey@winston.sh>
2023-10-14 14:31:52 +0200

    chore: remove nvim lua config (separate flake now)

--- home/apps/neovim/lua/config/init.lua
+++
@@ -1,4 +0,0 @@
-require("config.autocmds")
-require("config.mappings")
-require("config.options")
-require("config.plugins").setup({ { import = "plugins" } })
--- home/apps/neovim/lua/plugins/vanity.lua
+++
@@ -1,4 +0,0 @@
----@type LazyPluginSpec[]
-return {
-  "wakatime/vim-wakatime",
-}
