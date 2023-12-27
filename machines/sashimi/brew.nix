c45649050112923c708262427ecbb08b69d4cb5c
winston <hey@winston.sh>
2023-12-27 23:57:14 +0100

    feat(brew): sync, tweak activation settings, refactor

--- machines/sashimi/brew.nix
+++ machines/sashimi/brew.nix
@@ -1,58 +1,55 @@
-{config, ...}: {
+{config, ...}: let
+  noQuarantine = name: {
+    inherit name;
+    args.no_quarantine = true;
+  };
+  skipSha = name: {
+    inherit name;
+    args.require_sha = false;
+  };
+in {
   # make brew available in PATH
   environment.systemPath = [config.homebrew.brewPrefix];
 
   homebrew = {
     enable = true;
     caskArgs.require_sha = true;
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "uninstall";
-      upgrade = true;
-    };
-    casks = let
-      skipSha = name: {
-        inherit name;
-        args = {require_sha = false;};
-      };
-      noQuarantine = name: {
-        inherit name;
-        args = {no_quarantine = true;};
-      };
-    in [
+    casks = [
       "1password"
+      (skipSha "affinity-designer")
+      (skipSha "affinity-photo")
+      (skipSha "affinity-publisher")
       "alfred"
+      "azure-data-studio"
       "bitwarden"
       "blender"
+      (noQuarantine "easy-move-plus-resize")
+      (skipSha "element")
+      "elgato-wave-link"
       "eloston-chromium"
       "iina"
       "imageoptim"
       "jetbrains-toolbox"
-      "karabiner-elements"
       "keka"
       "little-snitch"
       "macfuse"
       "mattermost"
+      "mullvad-browser"
       "mullvadvpn"
       "obs"
-      "orion"
       "postman"
       "rustdesk"
+      "signal"
+      (skipSha "sizzy")
+      "uhk-agent"
       "uninstallpkg"
       "utm"
       "yubico-yubikey-manager"
-      (noQuarantine "easy-move-plus-resize")
-      (skipSha "affinity-designer")
-      (skipSha "affinity-photo")
-      (skipSha "affinity-publisher")
-      (skipSha "element")
-      (skipSha "sizzy")
-
-      # drivers
-      "elgato-wave-link"
-      "uhk-agent"
-      (noQuarantine "vial")
     ];
+    onActivation = {
+      autoUpdate = true;
+      upgrade = true;
+    };
     taps = ["homebrew/cask"];
   };
 }
