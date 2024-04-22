3bed58be25118c819aff9e5c71d6b128ab107fdc
winston <hey@winston.sh>
2024-04-22 12:34:14 +0200

    fix(network): ipv6 dns

--- machines/common/linux/network.nix
+++ machines/common/linux/network.nix
@@ -26,17 +26,25 @@
     };
     stubby = {
       enable = true;
-      settings = {
-        resolution_type = "GETDNS_RESOLUTION_STUB";
-        listen_addresses = ["127.0.0.1@53000" "0::1@53000"];
-        upstream_recursive_servers = [
-          {
-            address_data = "146.255.56.98";
-            tls_port = 853;
-            tls_auth_name = "dot1.applied-privacy.net";
-          }
-        ];
-      };
+      settings =
+        pkgs.stubby.passthru.settingsExample
+        // {
+          resolution_type = "GETDNS_RESOLUTION_STUB";
+          listen_addresses = [
+            "127.0.0.1@53000"
+            "0::1@53000"
+          ];
+          upstream_recursive_servers = [
+            {
+              address_data = "146.255.56.98";
+              tls_auth_name = "dot1.applied-privacy.net";
+            }
+            {
+              address_data = "2a01:4f8:c0c:83ed::1";
+              tls_auth_name = "dot1.applied-privacy.net";
+            }
+          ];
+        };
     };
   };
 }
