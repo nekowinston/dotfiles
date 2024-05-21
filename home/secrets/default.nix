74068173aabf8439fea3a4d156a30d8d6ad6f882
winston <hey@winston.sh>
2024-05-21 17:54:05 +0200

    feat(secrets): simplify agenix-hm on darwin

--- home/secrets/default.nix
+++ home/secrets/default.nix
@@ -1,28 +1,11 @@
+{ lib, ... }:
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  inherit (pkgs.stdenv) isDarwin;
-in
-{
-  age = {
-    identityPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
-
-    secrets =
-      with builtins;
-      listToAttrs (
-        map (k: {
-          name = lib.removePrefix "home/secrets/" (lib.removeSuffix ".age" k);
-          value = {
-            file = ./../.. + "/${k}";
-          };
-        }) (attrNames (import ./secrets.nix))
-      );
-
-    secretsDir = lib.mkIf isDarwin "/private/tmp/agenix";
-    secretsMountPoint = lib.mkIf isDarwin "/private/tmp/agenix.d";
-  };
+  age.secrets = builtins.listToAttrs (
+    builtins.map (k: {
+      name = lib.removePrefix "home/secrets/" (lib.removeSuffix ".age" k);
+      value = {
+        file = ./../.. + "/${k}";
+      };
+    }) (builtins.attrNames (import ./secrets.nix))
+  );
 }
