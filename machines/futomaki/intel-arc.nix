13d6bb4a361860771543c2dfd23826777813c550
winston <hey@winston.sh>
2025-02-09 16:55:24 +0100

    feat(futomaki): use intel-gpu-tools

--- machines/futomaki/intel-arc.nix
+++ machines/futomaki/intel-arc.nix
@@ -6,13 +6,16 @@
     LIBVA_DRIVER_NAME = "iHD";
     VDPAU_DRIVER = "va_gl";
   };
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-compute-runtime
-      intel-media-driver # modern VAAPI for Arc
-      libvdpau-va-gl # support VDPAU via VAAPI
-      vpl-gpu-rt
-    ];
+  hardware = {
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        intel-compute-runtime
+        intel-media-driver # modern VAAPI for Arc
+        libvdpau-va-gl # support VDPAU via VAAPI
+        vpl-gpu-rt
+      ];
+    };
+    intel-gpu-tools.enable = true;
   };
 }
