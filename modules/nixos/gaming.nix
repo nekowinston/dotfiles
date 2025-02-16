0eb51a192e30591c6d8e8e60a3e70f066f1fae86
winston <hey@winston.sh>
2025-02-16 18:29:22 +0100

    feat(gaming): bunch of tweaks

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -6,33 +6,51 @@
 }:
 let
   cfg = config.dotfiles.gaming;
+  notify-send = lib.getExe' pkgs.libnotify "notify-send";
+  systemctl = lib.getExe' pkgs.systemd "systemctl";
 in
 {
   options.dotfiles.gaming.enable = lib.mkEnableOption "gaming configuration";
 
   config = lib.mkIf cfg.enable {
-    # enable 32bit support for Steam
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
-    services.pulseaudio.support32Bit = true;
+    environment.systemPackages = with pkgs; [
+      (heroic.override {
+        extraPkgs = p: [
+          p.gamemode
+          p.mangohud
+        ];
+      })
+      mangohud
+      mangojuice
+      umu-launcher
+    ];
+
+    # extra group to allow gamemode to park CPU cores
+    users.users.${config.dotfiles.username}.extraGroups = [ "gamemode" ];
 
     programs = {
       gamemode = {
         enable = true;
         settings = {
-          custom = {
-            start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
-            end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
-          };
+          general.renice = 20;
+          cpu.park_cores = true;
+          custom.start =
+            (pkgs.writeShellScript "gamemode-start" ''
+              ${systemctl} stop --user wlsunset
+              ${notify-send} 'GameMode started'
+            '').outPath;
+          custom.end =
+            (pkgs.writeShellScript "gamemode-end" ''
+              ${systemctl} start --user wlsunset
+              ${notify-send} 'GameMode ended'
+            '').outPath;
         };
       };
       steam = {
         enable = true;
         extraPackages = with pkgs; [
-          corefonts
-          gamescope
+          gamemode
+          liberation_ttf
           mangohud
           wineWowPackages.staging
           winetricks
