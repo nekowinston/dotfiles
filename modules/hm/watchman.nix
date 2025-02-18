5c163952acb3c82472940e3eee54308f1041feb2
winston <hey@winston.sh>
2025-02-18 14:09:28 +0100

    fix(watchman): service definitions

--- modules/hm/watchman.nix
+++ modules/hm/watchman.nix
@@ -20,38 +20,37 @@ in
   config = lib.mkIf cfg.enable {
     home = {
       packages = [ cfg.package ];
-      sessionVariables.WATCHMAN_SOCK = lib.mkIf isLinux "$XDG_RUNTIME_DIR/watchman-sock";
+      sessionVariables = lib.mkIf isLinux {
+        WATCHMAN_SOCK = "$XDG_RUNTIME_DIR/watchman.sock";
+      };
     };
 
     # see https://github.com/facebook/watchman/commit/2985377eaf8c8538b28fae9add061b67991a87c2
     systemd.user = lib.mkIf isLinux {
       sockets.watchman = {
-        Unit.Description = "Watchman socket for user %i";
+        Unit.Description = "Watchman socket";
         Socket = {
           Accept = false;
-          ListenStream = "%t/watchman-sock";
+          ListenStream = "%t/watchman.sock";
           SocketMode = "0664";
         };
         Install.WantedBy = [ "sockets.target" ];
       };
 
       services.watchman = {
-        Unit = {
-          Description = "Watchman service for user %i";
-          After = [ "remote-fs.target" ];
-          Conflicts = [ "shutdown.target" ];
-        };
+        Unit.Description = "Watchman service";
         Service = {
           ExecStart = lib.concatStringsSep " " [
-            "${lib.getExe cfg.package}"
+            (lib.getExe cfg.package)
             "--foreground"
             "--inetd"
-            "--sockname=${config.systemd.user.sockets.watchman.Socket.ListenStream}"
+            "--unix-listener-path"
+            config.systemd.user.sockets.watchman.Socket.ListenStream
           ];
           Restart = "on-failure";
           StandardInput = "socket";
         };
-        Install.WantedBy = [ "multi-user.target" ];
+        Install.WantedBy = [ "default.target" ];
       };
     };
   };
