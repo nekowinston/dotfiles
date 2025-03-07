016ccc2061e2a48e608940e2c5b976365fb853d8
winston <hey@winston.sh>
2025-03-07 15:22:51 +0100

    feat(watchman): reenable service for Linux
    unlike macOS, the watchman binary doesn't spawn a system spawner service for us

--- modules/hm/watchman.nix
+++ modules/hm/watchman.nix
@@ -27,5 +27,34 @@ in
           WATCHMAN_SOCK = "/tmp/watchman.${config.home.username}.sock";
         };
     };
+
+    # see https://github.com/facebook/watchman/commit/2985377eaf8c8538b28fae9add061b67991a87c2
+    systemd.user = lib.mkIf isLinux {
+      sockets.watchman = {
+        Unit.Description = "Watchman socket";
+        Socket = {
+          Accept = false;
+          ListenStream = "%t/watchman.sock";
+          SocketMode = "0664";
+        };
+        Install.WantedBy = [ "sockets.target" ];
+      };
+
+      services.watchman = {
+        Unit.Description = "Watchman service";
+        Service = {
+          ExecStart = lib.concatStringsSep " " [
+            (lib.getExe cfg.package)
+            "--foreground"
+            "--inetd"
+            "--unix-listener-path"
+            config.systemd.user.sockets.watchman.Socket.ListenStream
+          ];
+          Restart = "on-failure";
+          StandardInput = "socket";
+        };
+        Install.WantedBy = [ "default.target" ];
+      };
+    };
   };
 }
